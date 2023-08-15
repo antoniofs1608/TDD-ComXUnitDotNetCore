4977691748720742fa9e252935c6fed1d3f2fc9f
Antonio Silva <antoniofranciscosilva.sp@gmail.com>
2023-08-14 21:40:53 -0300

    Adicionar projeto de CursoOnline para fazer o TDD

---
+++ CursoOnline/CursoOnline.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using CursoOnline.Domain.Curso;
+using CursoOnline.Domain.Curso.Enums;
+
+namespace CursoOnline.Domain.Test.Cursos
+{
+    public class CursoTest
+    {
+        // Criar um curso com os seguintes dados:
+        // Nome, Carga Horária, público alvo e valor do Curso
+
+        [Fact]
+        public void Curso_CriarCurso_RetornaCurso()
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Curso de TDD",
+                CargaHoraria = (double)40,
+                PublicoAlvo = PublicoAlvo.Estudante,
+                ValorDoCurso = (decimal)100
+            };
+
+            var curso = new Curso.Curso(CursoEsperado.Nome, CursoEsperado.CargaHoraria, CursoEsperado.PublicoAlvo, CursoEsperado.ValorDoCurso);
+
+            Assert.Equal(CursoEsperado.Nome, curso.Nome);
+        }
+    }
+}
---
+++ CursoOnline/CursoOnline.Domain/Curso/Curso.cs
@@ -0,0 +1,20 @@
+﻿using CursoOnline.Domain.Curso.Enums;
+
+namespace CursoOnline.Domain.Curso
+{
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public decimal ValorDoCurso { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, PublicoAlvo publicoAlvo, decimal valorDoCurso)
+        {
+            this.Nome = nome;
+            this.CargaHoraria = cargaHoraria;
+            this.PublicoAlvo = publicoAlvo;
+            this.ValorDoCurso = valorDoCurso;
+        }
+    }
+}
